04cb5c2a67b0eb9e8599982118d435482067b1ef
LukasEins <88452621+LukasEins@users.noreply.github.com>
2023-02-19 16:22:43 +0100

    Update walltimechanger.py

--- i3/scripts/walltimechanger.py
+++ i3/scripts/walltimechanger.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python
 
 import os
-import time
+import datetime
 
-# Define the path to your wallpaper directory
-WALLPAPER_DIR = '/home/lukas/Pictures/wallpaper/'
+def change_wallpaper():
+        hour = datetime.datetime.now().hour
+    if hour >= 6 and hour < 12:
+                wallpaper = "/home/lukas/Pictures/wallpaper/morning.png"
+    elif hour >= 12 and hour < 18:
+                wallpaper = "/home/lukas/Pictures/wallapper/afternoon.png"
+    elif hour >= 18 and hour < 22:
+                wallpaper = "/home/lukas/Pictures/wallpaper/evening.png"
+    else:
+                 wallpaper = "/home/lukas/Pictures/wallpaper/night.png"
+    os.system("feh --bg-fill {}".format(wallpaper))
+    os.system("pkill -USR1 feh")
 
-# Define the wallpaper images for each time of day
-WALLPAPERS = {
-    'morning': 'morning.jpg',
-    'afternoon': 'afternoon.png',
-    'evening': 'evening.jpg',
-    'night': 'night.png'
-}
-
-# Get the current time of day
-hour = time.localtime().tm_hour
-
-if 5 <= hour < 12:
-    wallpaper = WALLPAPERS['morning']
-elif 12 <= hour < 18:
-    wallpaper = WALLPAPERS['afternoon']
-elif 18 <= hour < 22:
-    wallpaper = WALLPAPERS['evening']
-else:
-    wallpaper = WALLPAPERS['night']
-
-# Set the wallpaper using feh
-os.system(f"feh --bg-scale {WALLPAPER_DIR}/{wallpaper}")
-
+if __name__ == "__main__":
+        change_wallpaper()
